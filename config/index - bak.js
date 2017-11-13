7aa1402c658669bcf30175bb0f7f03e5169fc545
leo-lai <leolai.mail@qq.com>
2017-11-13 10:49:00 +0800

    项目配置副本

---
+++ config/index - bak.js
@@ -0,0 +1,49 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+//【apps】，默认为apps，修改这里配置的同时，也要同时重命名/src/apps的这个文件夹名称  
+// 对应编译后部署到服务器的路径是 http://192.168.0.100:8085/apps/default/index.html
+var host = 'localhost'
+var port = 8090
+var moduleName = 'xifengniao'
+
+
+module.exports = {
+  moduleName,
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist/' + moduleName),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '../',
+    productionSourceMap: true,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    host: host,
+    port: port,
+    autoOpenBrowser: true,
+    index: path.resolve(__dirname, 'index.html'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+    },
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
+  }
+}
